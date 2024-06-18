35ba14d59c56acdf8f6af40425a339e51134fa15
Collins Nkemdilim <collinsnkemdilim05@gmail.com>
2024-06-18 10:32:52 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById('hamburger-menu');
-const links = document.getElementById('links');
-
-menu.addEventListener('click', () => {
-  links.classList.toggle('hidden');
-});
